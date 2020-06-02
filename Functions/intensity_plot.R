c8f68c330cec3a3c086e437b4812ae0bd84d68ee
cheesesnakes <shawn27.dsouza@gmail.com>
2020-06-02 16:38:12 +0530

    Updated diet analysis, annotated script and created final figure 2

--- Functions/intensity_plot.R
+++ Functions/intensity_plot.R
@@ -12,11 +12,7 @@ int.plot <- function(mdf, gear, name, title.face = NULL){
                         breaks=seq(-10, -80, -10), colour="black", size = 0.1, linetype = "dashed")+
     metR::geom_text_contour(data = depth, aes(x = lon, y = lat, z = depth), skip = 1,
                             parse = T,check_overlap = T, size = 1)+
-<<<<<<< HEAD
     geom_raster(data = mdf, aes(x = x, y = y, fill =layer), alpha = 0.8, color = "grey50")+
-=======
-    geom_raster(data = mdf, aes(x = x, y = y, fill =layer), alpha = 0.6)+
->>>>>>> 7becf1bfcfd46d12d0332f3098398155f4c02dd2
     coord_quickmap()+
     scale_fill_viridis(name = name, option = "B")+
     #scale_y_continuous(limits = c(73.1, 73.8))+
