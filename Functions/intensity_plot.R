f8344bebc01bbe06f2b933bf6ca1fb26884222b7
cheesesnakes <shawn27.dsouza@gmail.com>
2020-06-02 13:44:57 +0530

    Updated depth data and improved intensity plot

--- Functions/intensity_plot.R
+++ Functions/intensity_plot.R
@@ -1,6 +1,6 @@
 #Function for creating plot for each Gear/Species
 
-int.plot <- function(mdf, gear, name){
+int.plot <- function(mdf, gear, name, title.face = NULL){
   
   require(ggmap)
   require(ggplot2)
@@ -9,16 +9,15 @@ int.plot <- function(mdf, gear, name){
   
   ggmap(base)+
     metR::geom_contour2(data = depth, aes(x = lon, y = lat, z = depth),
-                        breaks=seq(-10, -80, -10), colour="black", size = 0.3
-    )+
+                        breaks=seq(-10, -80, -10), colour="black", size = 0.1, linetype = "dashed")+
     metR::geom_text_contour(data = depth, aes(x = lon, y = lat, z = depth), skip = 1,
-                            parse = T,check_overlap = T, size = 3)+
-    geom_raster(data = mdf, aes(x = x, y = y, fill =layer), alpha = 0.6)+
+                            parse = T,check_overlap = T, size = 1)+
+    geom_raster(data = mdf, aes(x = x, y = y, fill =layer), alpha = 0.8, color = "grey50")+
     coord_quickmap()+
-    scale_fill_viridis(name = name, direction = -1)+
+    scale_fill_viridis(name = name, option = "B")+
     #scale_y_continuous(limits = c(73.1, 73.8))+
     labs(title = gear, x = "Longitude", y = "Latitude")+
-    theme(plot.title = element_text(hjust = 0.5),
+    theme(plot.title = element_text(hjust = 0.5, face = title.face),
           legend.text = element_text(size = 10),
           text = element_text(size = 15))
 }
