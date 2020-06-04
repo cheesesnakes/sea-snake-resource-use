d0cbb804ea7f896f6c99d943bffae9b7e224a19b
cheesesnakes <shawn27.dsouza@gmail.com>
2020-06-04 14:03:27 +0530

    Updated geocoding protocol and functions

--- Functions/geocode.R
+++ Functions/geocode.R
@@ -1,5 +1,20 @@
 # Protocol for geocoding boat trips
 
+## importing bathymetric data downloaded from GEBCO database
+
+library(marmap)
+library(tidyverse)
+
+### raw depth data
+
+depth <- readGEBCO.bathy("./Data/gebco_2020_n16.6_s15.6_w72.9_e73.9.nc")
+
+### converting to data frame
+
+depth = fortify.bathy(depth)%>%
+  rename(lon = x, lat = y, depth = z)%>%
+  filter(depth < 1)
+
 ## matching funtions for theta join
 
 depth_match <- function(x,y){
@@ -13,61 +28,41 @@ lat_match <- function(x,y){
 
 # geocoding function
 
-geocode.trips <- function(df, fl, dep) {
-  # Geocoding local intensity data with depth
-  df = df%>%
-    ungroup()%>%
-    mutate_(fl = as.character(fl))%>%
-    mutate_geocode(location = fl)
+geocode.trips <- function(df, fl, dep, trip = NULL) {
   
-  # Extracting coordinates from depth data
+  # dependencies
   
+  require(ggmap)
   require(fuzzyjoin)
   
-  df <- df%>%
-    mutate_(depth = dep)%>%
-    fuzzy_inner_join(y = depth, by = c("depth", "lat"), 
-                     match_fun = list(depth = depth_match,
-                                      lat = lat_match))
-    rename(lon = lon.y,# longitude from fishing location
-           lat = lat.x)# latitude from depth data
+  # Geocoding fishing location
+  
+  df <- df%>% ungroup() 
+  
+  df <-  mutate_(df, fl = as.character(fl))
+  
+  loc <- select(df, fl)%>%distinct() # isolating location for faster computation
     
-  df <- distinct(df, Date, Boat.name, .keep_all = T)# removing introduced duplicates
+  loc  <- mutate_geocode(loc, location = fl)
+  
+  df <- left_join(df, loc)
+    
+  # Extracting coordinates from depth data
+
+  df <- mutate_(df, depth = dep) # renaming depth variable
+  
+  df <- fuzzy_inner_join(df, y = depth, by = c("depth", "lat"), 
+                         match_fun = list(depth = depth_match, lat = lat_match))
+    
+  df <- rename(df, lon = lon.y,# longitude from fishing location
+           lat = lat.x)# latitude from depth data
+  
+  df <- dplyr::select(df, -c(lon.x, lat.y, depth.x, depth.y, fl))
+  
+  if (!is.null(trip)) {
+    
+    df <- distinct_(df, 'Date', trip, .keep_all = T)# removing introduced duplicates
+  }  
+  
+  return(df)
 }
-
-# sea snake distribution example
-
-#Geocoding sea snake density
-
-snakes_den = snakes_den%>%
-  ungroup()%>%
-  mutate(Fishing.Location = as.character(Fishing.Location))%>%
-  mutate_geocode(location = Fishing.Location)
-
-#Extracting point data from depth and location
-
-library(fuzzyjoin)
-
-#matching funtions for theta join
-
-depth_match <- function(x,y){
-  x <- -x
-  x <= y+0.5 & x >= y-0.5 #interval for theta matching depth
-}
-
-lat_match <- function(x,y){
-  x <= y+0.005 & x >= y-0.005 #interval for theta matching latitude
-}
-
-snakes_den <- snakes_den%>%
-  mutate(depth = Depth.Caught..m.)%>%
-  fuzzy_inner_join(y = depth, by = c("depth", "lat"), 
-                   match_fun = list(depth = depth_match,
-                                    lat = lat_match))%>%
-  dplyr::select(Date, Boat.Name.Owner, Species, Fishing.Location, Depth.Caught..m., 
-                lon.y, lat.x, CPUE, n)%>%#dplyr::selecting relevant variables
-  rename(lon = lon.y,# longitude from fishing location
-         lat = lat.x)%>%# latitude from depth data
-  distinct(Date, Boat.Name.Owner, Species, Fishing.Location, Depth.Caught..m., .keep_all = T)
-
-write.csv(snakes_den, "./Data/snakes-density.csv")#saved data set to reduce compute time
