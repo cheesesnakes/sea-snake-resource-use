d4e19db36f7473f8def98e1258bd8d77687ff480
Shawn Dsouza <shawn27.dsouza@gmail.com>
2020-10-19 15:46:34 +0530

    Combining SIA data

--- Functions/clean sia data.R
+++ Functions/clean sia data.R
@@ -23,3 +23,12 @@ snakes = read.csv("./Data/Sea-snakes_fish-dep_2018-19_250720.csv")
 sia_samps <- sia_new%>%
   left_join(snakes)%>%
   select(Field.Code:Species, Gear.Type, Gut.Content, Blood, Scale)
+
+## Combining SIA data
+
+sia_old <- read.csv("./Data/Stable Isotope Data_CEAS_241119.csv")
+
+sia_final <- sia_old%>%
+  full_join(sia_new)
+
+write.csv(sia_final, "./Data/Stable Isotope Data_CEAS_final_191020.csv")
