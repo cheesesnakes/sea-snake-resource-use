fb37fd13addfbaa648a97bedff957c29dc1b8a0d
Shawn Dsouza <cheesesnakes@remote-compute.asia-south1-c.c.secret-bay-314613.internal>
2021-05-29 06:37:20 +0000

    Fixed issue with intesity plot (metR::text_contour)

--- Functions/intensity_plot.R
+++ Functions/intensity_plot.R
@@ -11,15 +11,14 @@ int.plot <- function(mdf, gear, name, title.face = NULL){
   ggmap(base)+
     metR::geom_contour2(data = depth, aes(x = lon, y = lat, z = depth),
                         breaks=seq(-10, -1000, -10), colour="black", size = 0.1, linetype = "dashed")+
-    metR::geom_text_contour(data = depth, aes(x = lon, y = lat, z = depth), skip = 1,
-                            parse = T, check_overlap = T, size = 1)+
+    #metR::geom_text_contour(data = depth, aes(x = lon, y = lat, z = depth), skip = 1,
+     #                       parse = T, check_overlap = T, size = 1)+
     geom_raster(data = mdf, aes(x = x, y = y, fill = layer, alpha = layer), color = "grey50")+
     coord_quickmap()+
     scale_x_continuous(limits = c(72.75, 73.75))+
     scale_y_continuous(limits = c(15.6, 16.6))+
     scale_fill_viridis(name = name, option = "B")+
     scale_alpha_continuous(range = c(0.7, 0.8), guide = F)+
-    #scale_y_continuous(limits = c(73.1, 73.8))+
     labs(title = gear, x = "Longitude", y = "Latitude")+
     theme(plot.title = element_text(hjust = 0.5, face = title.face),
           legend.text = element_text(size = 10),
