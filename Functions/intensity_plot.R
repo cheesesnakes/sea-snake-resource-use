2057a59e88baf72582353dae0c3e5a056821c964
Shawn Dsouza <shawn27.dsouza@gmail.com>
2021-05-29 18:22:47 +0530

    Updated fig 1 for better vis

--- Functions/intensity_plot.R
+++ Functions/intensity_plot.R
@@ -13,7 +13,7 @@ int.plot <- function(mdf, gear, name, title.face = NULL){
                         breaks=seq(-10, -1000, -10), colour="black", size = 0.1, linetype = "dashed")+
     #metR::geom_text_contour(data = depth, aes(x = lon, y = lat, z = depth), skip = 1,
      #                       parse = T, check_overlap = T, size = 1)+
-    geom_raster(data = mdf, aes(x = x, y = y, fill = layer, alpha = layer), color = "grey50")+
+    geom_raster(data = mdf, aes(x = x, y = y, fill = layer), color = "grey50", alpha = 0.8)+
     coord_quickmap()+
     scale_x_continuous(limits = c(72.75, 73.75))+
     scale_y_continuous(limits = c(15.6, 16.6))+
